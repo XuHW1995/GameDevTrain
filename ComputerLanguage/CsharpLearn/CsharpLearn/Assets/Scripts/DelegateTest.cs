742bb24cff79163246781cce1ada5243ced7ab5d
Xuhongwei <790543974@qq.com>
2021-09-07 22:22:37 +0800

    delegate and event exercise

---
+++ ComputerLanguage/CsharpLearn/CsharpLearn/Assets/Scripts/DelegateTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public delegate int TestCalc(int x, int y);
+    
+    public class DelegateTest : MonoBehaviour
+    {
+        public void Start()
+        {
+            Calculator c = new Calculator();
+            
+            //内置委托，无返回值
+            Action action1 = new Action(c.Fun1);
+            action1.Invoke();
+            action1();
+            Action<int, int> action2 = new Action<int, int>(c.Fun2);
+            action2(100, 200);
+            //内置委托有返回值
+            Func<int, int, int> fun1 = new Func<int, int, int>(c.Fun3);
+            int result = fun1(100, 200);
+            Debug.Log(result);
+            
+            //自定义委托
+            TestCalc tc = new TestCalc(c.Fun3);
+            Debug.Log(tc(200, 100));
+            
+            //自定义委托模板方法调用方式
+            Debug.Log("模板方法调用"+ TemplateFun(tc));
+            //自定义委托回调调用方式
+            CallBackFun(tc);
+            
+            //多播委托
+            TestCalc tc2 = new TestCalc(c.Fun3);
+            tc += tc2;
+            Debug.Log(tc(200, 100));
+            
+            //同步调用
+            //直接调用
+            c.Fun1();
+            Thread.Sleep(1000);
+            c.Fun2(100,1);
+            Thread.Sleep(1000);
+            c.Fun3(100, 2);
+            Thread.Sleep(1000);
+            
+            //通过委托进行间接同步调用
+            tc(100, 2);
+            
+            //委托的隐式异步调用
+            
+            //显示异步调用 多线程
+
+        }
+
+        private int TemplateFun(TestCalc tc)
+        {
+            int templateFunResult = tc(100, 200);
+            return templateFunResult + 1;
+        }
+
+        private void CallBackFun(TestCalc tcCallbacl)
+        {
+            int x = 100;
+            int y = 200;
+            if (tcCallbacl != null)
+            {
+                tcCallbacl(x + y, x - y);
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public void Fun1()
+        {
+            Debug.Log("测试下哈哈哈哈");
+        }
+
+        public void Fun2(int a, int b)
+        {
+            int all = a + b;
+            Debug.Log(all);
+        }
+
+        public int Fun3(int a, int b)
+        {
+            Debug.Log("Fun3 invoke");
+            return a - b;
+        }
+        
+        public int Fun4(int a, int b)
+        {
+            Debug.Log("Fun4 invoke");
+            return a + b;
+        }
+    }
+}
---
+++ ComputerLanguage/CsharpLearn/CsharpLearn/Assets/Scripts/EventTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using UnityEngine;
+using  System.Timers;
+
+namespace DefaultNamespace
+{
+    public class EventTest : MonoBehaviour
+    {
+        Timer timer = new Timer();
+        public void Start()
+        {
+
+            // timer.Interval = 1000;
+            //
+            // EventHandler1 eh1 = new EventHandler1();
+            // timer.Elapsed += eh1.TimerHandler;
+            //
+            // EventHandler2 eh2 = new EventHandler2();
+            // timer.Elapsed += eh2.Sing;
+            //
+            // timer.Start();
+
+            
+            Customer c = new Customer();
+            Waiter w = new Waiter();
+            //事件订阅
+            c.Order += w.Action;
+            c.eventOrder += w.Action;
+            c.eh2 += w.Action;
+            c.BeginOrder();
+            c.PayBill();
+        }
+
+        private void OnDisable()
+        {
+            timer.Stop();
+        }
+    }
+
+    public class OrderEventArgs : EventArgs
+    {
+        public string name;
+        public string size;
+    }
+
+    //委托类型的事件处理器
+    public delegate void OrderEventHandler(Customer customer, OrderEventArgs e);
+    
+    //事件拥有者
+    public class Customer
+    {
+        private OrderEventHandler orderEventHandler;
+        //事件旧的声明方式
+        public event OrderEventHandler Order
+        {
+            add
+            {
+                this.orderEventHandler += value;
+            }
+
+            remove
+            {
+                this.orderEventHandler -= value;
+            }
+        }
+
+        //简化版事件声明方式
+        public event OrderEventHandler eventOrder;
+        
+        //委托声明方式
+        public OrderEventHandler eh2;
+        
+        public double bill { get; set; }
+
+        public void PayBill()
+        {
+            Debug.Log("付钱" + bill);
+        }
+
+        public void Walkin()
+        {
+            Debug.Log("客人进来啦");
+            
+        }
+
+        public void BeginOrder()
+        {
+            Debug.Log("开始点菜");
+
+            OrderEventArgs e = new OrderEventArgs();
+            e.name = "水煮肉";
+            e.size = "Big";
+            this.orderEventHandler(this, e);
+
+            e.name = "土豆丝";
+            e.size = "Small";
+            this.eventOrder(this, e);
+            
+            e.name = "炒黄瓜";
+            e.size = "Small";
+            this.eh2(this, e);
+        }
+    }
+
+    //事件响应者
+    public class Waiter
+    {
+        //事件处理器
+        public void Action(Customer customer, OrderEventArgs e)
+        {
+            Debug.Log("服务员上菜"+  e.name);
+            int price = 0;
+            switch (e.size)
+            {
+                case "Small":
+                    price = 10;
+                    break;
+                case "Big":
+                    price = 20;
+                    break;
+                default:
+                    price = 5;
+                    break;
+            }
+
+            customer.bill += price;
+        }
+    }
+    
+    
+    public class EventHandler1
+    {
+        public void TimerHandler(object sender, ElapsedEventArgs e)
+        {
+            Debug.Log("EventHandler1 响应");
+        }
+    }
+
+    public class EventHandler2
+    {
+        public void Sing(object sender, ElapsedEventArgs e)
+        {
+            Debug.Log("EventHandler2 响应唱歌");
+        }
+    }
+
+    public class EventHandler3
+    {
+        
+    }
+}
