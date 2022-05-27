f720eff2317dd93b05dea30af5197d35a2c64a81
Xuhongwei <790543974@qq.com>
2022-05-27 15:45:13 +0800

    94 二叉树中序遍历

---
+++ Leetcode/94.二叉树的中序遍历.cs
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=csharp
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Easy (75.87%)
+ * Likes:    1446
+ * Dislikes: 0
+ * Total Accepted:    833K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    List<int> result = new List<int>();
+    public IList<int> InorderTraversal(TreeNode root) {
+        if(root == null)
+        {
+            return result ;
+        }
+        InorderTraversal(root.left);
+        result.Add(root.val);
+        InorderTraversal(root.right);
+
+        return result;
+    }
+}
+// @lc code=end
+
