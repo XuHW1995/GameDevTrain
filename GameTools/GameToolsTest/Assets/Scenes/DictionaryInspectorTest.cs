6fef0f764ba1275ed989b5f2b28d2ebd2ff4c1a5
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2022-07-25 14:23:48 +0800

    GameTools test project

---
+++ GameTools/GameToolsTest/Assets/Scenes/DictionaryInspectorTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+
+public class Person
+{
+    public int age;
+    public string name;
+    public MovementAblitiy movementAblitiy;
+
+    public override string ToString()
+    {
+        return $"{name} is {age} old, has {movementAblitiy.ToString()}";
+    }
+}
+
+public class MovementAblitiy
+{
+    public float moveSpeed;
+    public bool moveAble;
+
+    public override string ToString()
+    {
+        return $"{moveAble} movespeed = {moveSpeed}";
+    }
+}
+
+public class DictionaryInspectorTest : SerializedMonoBehaviour
+{
+
+    public Dictionary<int, Person> personMap = new Dictionary<int, Person>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("打印person")]
+    public void DebugPerson()
+    {
+        foreach (var personKeyValue in personMap)
+        {
+            Debug.Log(personKeyValue.Value.ToString());
+        }
+    }
+}
---
+++ GameTools/GameToolsTest/Assets/Scenes/SelfSerialztion.cs
@@ -0,0 +1,28 @@
+﻿/******************************************************************************
+* Created by: XuHongWei
+* Date: 2022-07-25 14:05:34
+* Des: 
+*******************************************************************************/
+
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+
+[ShowOdinSerializedPropertiesInInspector]
+public class SelfSerialztion : MonoBehaviour, ISerializationCallbackReceiver, ISupportsPrefabSerialization
+{
+    [SerializeField, HideInInspector]
+    private SerializationData serializationData;
+
+    SerializationData ISupportsPrefabSerialization.SerializationData { get { return this.serializationData; } set { this.serializationData = value; } }
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+        UnitySerializationUtility.DeserializeUnityObject(this, ref this.serializationData);
+    }
+
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+        UnitySerializationUtility.SerializeUnityObject(this, ref this.serializationData);
+    }
+}
