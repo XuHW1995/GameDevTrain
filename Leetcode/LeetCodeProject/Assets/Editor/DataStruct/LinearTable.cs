8b628837f8a2193591653a809d9d9201f9981a5b
Xuhongwei <790543974@qq.com>
2022-08-09 22:46:05 +0800

    linearTable exercise

---
+++ Leetcode/LeetCodeProject/Assets/Editor/DataStruct/LinearTable.cs
@@ -0,0 +1,34 @@
+/******************************************************************************
+* Created by: XuHongWei
+* Date: 2022-08-09 22:18:28
+* Des: 自定义线性表
+*******************************************************************************/
+
+public class LinearTable<TItem>
+{
+    private TItem[] _items;
+    public int Length
+    {
+        get { return _items.Length; }
+    }
+    
+    public void Insert(int index, TItem item)
+    {
+        
+    }
+
+    public void Delete(int index)
+    {
+        
+    }
+    
+    public TItem FindItem(int index)
+    {
+        return _items[index];
+    }
+
+    public int FindItemFirstIndex(TItem item)
+    {
+        return 0;
+    }
+}
