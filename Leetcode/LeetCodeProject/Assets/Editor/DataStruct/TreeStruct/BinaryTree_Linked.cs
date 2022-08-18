6fee48a355f9c9fe97409773b01f80126bc5caae
Xuhongwei <790543974@qq.com>
2022-08-18 22:47:48 +0800

    binarytree test

---
+++ Leetcode/LeetCodeProject/Assets/Editor/DataStruct/TreeStruct/BinaryTree_Linked.cs
@@ -0,0 +1,20 @@
+/******************************************************************************
+* Created by: XuHongWei
+* Date: 2022-08-18 22:36:36
+* Des: 二叉树链式存储
+*******************************************************************************/
+
+public class BinaryTreeNode
+{
+    public int data;
+    public BinaryTreeNode leftNode;
+    public BinaryTreeNode rightNode;
+}
+
+public class BinaryTree_Linked
+{
+    public static BinaryTree_Linked CreatEmptyTree()
+    {
+        return null;
+    }
+}
