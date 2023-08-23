17055213111c29449d44a65e09d9109d2b1fa467
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2023-08-23 17:54:27 +0800

    test playable api and try scriptplayable behavior

---
+++ GameAnimation/AnimancerTest/AnimancerTest/Assets/AnimancerController/AnimancerControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Animancer;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+
+namespace AnimancerController
+{
+    public class AnimancerControllerTest : MonoBehaviour
+    {
+        public Animator testAnimator;
+        public AnimationClip testClip;
+        
+        [Button]
+        public void InitGraph()
+        {
+            var graph = PlayableGraph.Create("XHWTestGraph");
+
+            var output = AnimationPlayableOutput.Create(graph, "XHWAniOutPut", testAnimator);
+
+            var clipPlayable = AnimationClipPlayable.Create(graph, testClip);
+
+            output.SetSourcePlayable(clipPlayable);
+            
+                
+            var out2 = ScriptPlayableOutput.Create(graph, "XHWCustomOutPut");
+            var selfPlayable = ScriptPlayable<TestScriptPlayableBehavior>.Create(graph, 1);
+            out2.SetSourcePlayable(selfPlayable);
+            
+            graph.Play();
+            
+            testAnimator.Play("haha");
+            
+        }
+    }
+}
---
+++ GameAnimation/AnimancerTest/AnimancerTest/Assets/AnimancerController/TestScriptPlayableBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace AnimancerController
+{
+    public class TestScriptPlayableBehavior : PlayableBehaviour
+    {
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            base.OnBehaviourPlay(playable, info);
+            Debug.Log("XHW OnBehaviourPlay");
+        }
+
+        public override void PrepareFrame(Playable playable, FrameData info)
+        {
+            base.PrepareFrame(playable, info);
+            Debug.Log("XHW PrepareFrame");
+        }
+    }
+}
