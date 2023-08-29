bd0596d80c1e768f1836e5ce5bf5d8e8da13bd0d
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2023-08-29 19:52:17 +0800

    xnode odin 新增

---
+++ GameAnimation/AnimancerTest/AnimancerTest/Assets/XNodeTest/TestXHWNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using XNode;
+
+public class TestXHWNode : Node {
+
+	[Input] public float a;
+	[Input,BoxGroup("xhw")] public float b;
+	// The value of an output node field is not used for anything, but could be used for caching output results
+	[Output] public float result;
+	[Output] public float sum;
+
+	// The value of 'mathType' will be displayed on the node in an editable format, similar to the inspector
+	public MathType mathType = MathType.Add;
+	public enum MathType { Add, Subtract, Multiply, Divide}
+    
+	// GetValue should be overridden to return a value for any specified output port
+	public override object GetValue(NodePort port) {
+
+		// Get new a and b values from input connections. Fallback to field values if input is not connected
+		float a = GetInputValue<float>("a", this.a);
+		float b = GetInputValue<float>("b", this.b);
+
+		// After you've gotten your input values, you can perform your calculations and return a value
+		if (port.fieldName == "result")
+			switch(mathType) {
+				case MathType.Add: default: return a + b;
+				case MathType.Subtract: return a - b;
+				case MathType.Multiply: return a * b;
+				case MathType.Divide: return a / b;
+			}
+		else if (port.fieldName == "sum") return a + b;
+		else return 0f;
+	}
+
+	[Button]
+	public void testButton()
+	{
+		
+	}
+
+	[BoxGroup("xhw")]
+	public float textXhw;
+}
