ee26abbfc233fce5c103558b62f1d3b228fb6541
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2024-01-10 19:31:48 +0800

    更新 odin enumerable test

---
+++ ComputerGraphics/CSRPLearn/CustomSRPLearn/Assets/TESTFPX/TestIEnumarator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace TESTFPX
+{
+    public class TestIEnumarator : MonoBehaviour
+    {
+        [Button]
+        public void TestEnumaratorCall()
+        {
+            IEnumerable<int> iterable = CreateEnumerable();
+            IEnumerator<int> iterator = iterable.GetEnumerator();
+            Debug.Log("Start to iterate");
+            while (true)
+            {
+                Debug.Log("Calling MoveNext()");
+                bool result = iterator.MoveNext();
+                Debug.Log($"MoveNext result = {result}");
+
+                if (!result)
+                {
+                    break;
+                }
+                
+                Debug.Log($"Fetching current = {iterator.Current}");
+            }
+        }
+
+        private readonly string padding = "【迭代器内部逻辑】";
+
+        IEnumerable<int> CreateEnumerable()
+        { 
+            Debug.Log($"{padding} Start of CreateEnumerable");
+            for (int i = 0; i < 3; i++)
+            {
+                Debug.Log($"{padding}Before to yield {i}");
+                yield return i;
+                Debug.Log($"{padding}After to yield {i}");
+            }
+            
+            Debug.Log($"{padding}Yielding final value");
+            yield return -1;
+            Debug.Log($"{padding}End of CreateEnumerable");
+        }
+    }
+}
